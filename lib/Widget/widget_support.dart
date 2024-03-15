d2bbbd2581143d1639eef617c4268678014dcf14
unknown <21521637@gm.uit.edu.vn>
2024-03-15 09:50:45 +0700

    Login Ui commit

---
+++ lib/Widget/widget_support.dart
@@ -0,0 +1,22 @@
+
+import 'package:flutter/material.dart';
+
+class AppWidget{
+  static TextStyle regularTextStyle() {
+    return const TextStyle(
+      color: Colors.black,
+      fontSize: 15,
+      fontWeight: FontWeight.bold,
+      fontFamily: 'Pacifico'
+    );
+  }
+
+  static TextStyle highlightTextStyle() {
+    return const TextStyle(
+        color: Colors.black,
+        fontSize: 30,
+        fontWeight: FontWeight.bold,
+        fontFamily: 'Pacifico'
+    );
+  }
+}
